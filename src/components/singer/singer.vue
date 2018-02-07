77bb52ccfe059931a02d8e95830f7bcea3bc3a9d
tycsbs <tutebaisong@163.com>
2018-02-07 17:05:06 +0800

    init bsMusic based on Vue stack

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="singer" ref="singer">
+    <list-view @select="selectSinger" :data="singers" ref="list"></list-view>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+//  const HOT_SINGER_LEN = 10
+//  const HOT_NAME = '热门'
+export default {}
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
+</style>
